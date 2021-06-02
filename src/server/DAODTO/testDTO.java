10325042f2a9e98e2955aff9ebf633ae1ab018d9
lsminkk <lsminkor@gmail.com>
2021-06-02 10:43:49 +0900

    서버 기본틀

---
+++ src/server/DAODTO/testDTO.java
@@ -0,0 +1,4 @@
+package server.DAODTO;
+
+public class testDTO {
+}
---
+++ src/server/Protocol/testProtocol.java
@@ -0,0 +1,9 @@
+package server.Protocol;
+
+import  server.Protocol.ProtocolType;
+import server.DAODTO.testDAO;
+import server.DAODTO.testDTO;
+
+public class testProtocol {
+
+}
